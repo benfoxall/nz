c9e4738389ebad81aa9ee5aafe1448f237475c5a
Ben Foxall <benfoxall@gmail.com>
2018-09-22 13:57:12 +0100

    Data!!

--- backend/handler.js
+++ backend/handler.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'eu-west-2' });
+
+const client = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+
+const params = {
+  TableName: 'nz-data',
+  Key: { 'id': 'test', 'time': 'now' }
+};
+
+module.exports.query = async (event, context) => {
+
+  const resp = await new Promise((resolve, reject) => {
+    client.get(params, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    });
+  })
+
+
+  return {
+    statusCode: 200,
+    body: "stuff: " + JSON.stringify(resp)
+  }
+}
+
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
